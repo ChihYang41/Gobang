dbfb1750f4ab714e98b40b88b1f6e8700aab79e6
ChihYang41 <Jigsaw0401@gmail.com>
2019-08-25 22:45:07 +0800

    Updates

---
+++ precache-manifest.49337fed5395633005d8832ce6be7607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d6a631ab13aa585c60bd244f0df5fb",
+    "url": "/Gobang/index.html"
+  },
+  {
+    "revision": "5587d0ba67d749210759",
+    "url": "/Gobang/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5acaf3b9de72c8f2bb9f",
+    "url": "/Gobang/static/js/2.18dce7a4.chunk.js"
+  },
+  {
+    "revision": "5587d0ba67d749210759",
+    "url": "/Gobang/static/js/main.8b67e0e6.chunk.js"
+  },
+  {
+    "revision": "bbbddfb177d1d98748b8",
+    "url": "/Gobang/static/js/runtime~main.8e94bc5f.js"
+  }
+]);
